b2ea2b437b40a7b4ad02407803c5372ee9633ae4
Your Name <you@example.com>
2025-02-20 15:10:29 -0300

    refactor ReproductionController to use request user for authorization; improve validation and handling of reproduction types in store method; add HasFactory trait and date casts in Reproduction model

--- app/Http/Controllers/ReproductionController.php
+++ app/Http/Controllers/ReproductionController.php
@@ -6,19 +6,19 @@ use App\Models\Animal;
 use App\Models\Reproduction;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\Auth;
 
 class ReproductionController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // Retorna os animais do usuário e as reproduções já cadastradas
-        $animals = Animal::where('user_id', Auth::id())->get();
+        $user = $request->user();
+        
+        $animals = Animal::where('user_id', $user->id)->get();
 
         $reproductions = Reproduction::with([
-                'egua','cavalo','doadora','receptor','animal','pai'
+                'egua', 'cavalo', 'doadora', 'receptor', 'animal', 'pai'
             ])
-            ->where('user_id', Auth::id())
+            ->where('user_id', $user->id)
             ->orderByDesc('id')
             ->get();
 
@@ -30,25 +30,27 @@ class ReproductionController extends Controller
 
     public function store(Request $request)
     {
+        // Validação básica; é interessante adicionar regras específicas para cada tipo
         $data = $request->validate([
-            'type' => 'required|string', // monta_natural, inseminacao, transferencia, confirmacao_prenhes
+            'type' => 'required|string|in:monta_natural,inseminacao,transferencia,confirmacao_prenhes',
+            'date' => 'required|date',
+            // Outras validações podem ser adicionadas conforme o tipo de reprodução
         ]);
 
-        // Campos comuns a todos
-        $data['user_id'] = Auth::id();
-        $data['date'] = $request->input('date');
+        // Atribui o usuário autenticado
+        $data['user_id'] = $request->user()->id;
 
         switch ($data['type']) {
             case 'monta_natural':
             case 'inseminacao':
-                // Campos: Nome da égua, Nome do cavalo e Data
-                // Confirmamos se o user escolheu 'useEguaRegistered' e 'useCavaloRegistered'
-                if ($request->input('useEguaRegistered') && $request->input('egua_id')) {
+                // Para a égua
+                if ($request->boolean('useEguaRegistered') && $request->filled('egua_id')) {
                     $data['egua_id'] = $request->input('egua_id');
                 } else {
                     $data['egua_name'] = $request->input('egua_name');
                 }
-                if ($request->input('useCavaloRegistered') && $request->input('cavalo_id')) {
+                // Para o cavalo
+                if ($request->boolean('useCavaloRegistered') && $request->filled('cavalo_id')) {
                     $data['cavalo_id'] = $request->input('cavalo_id');
                 } else {
                     $data['cavalo_name'] = $request->input('cavalo_name');
@@ -56,20 +58,20 @@ class ReproductionController extends Controller
                 break;
 
             case 'transferencia':
-                // Campos: Nome da doadora, Nome do Cavalo, Nome do receptor, Data
-                if ($request->input('useDoadoraRegistered') && $request->input('doadora_id')) {
+                // Para a doadora
+                if ($request->boolean('useDoadoraRegistered') && $request->filled('doadora_id')) {
                     $data['doadora_id'] = $request->input('doadora_id');
                 } else {
                     $data['doadora_name'] = $request->input('doadora_name');
                 }
-
-                if ($request->input('useCavaloRegistered') && $request->input('cavalo_id')) {
+                // Para o cavalo
+                if ($request->boolean('useCavaloRegistered') && $request->filled('cavalo_id')) {
                     $data['cavalo_id'] = $request->input('cavalo_id');
                 } else {
                     $data['cavalo_name'] = $request->input('cavalo_name');
                 }
-
-                if ($request->input('useReceptorRegistered') && $request->input('receptor_id')) {
+                // Para o receptor
+                if ($request->boolean('useReceptorRegistered') && $request->filled('receptor_id')) {
                     $data['receptor_id'] = $request->input('receptor_id');
                 } else {
                     $data['receptor_name'] = $request->input('receptor_name');
@@ -77,26 +79,23 @@ class ReproductionController extends Controller
                 break;
 
             case 'confirmacao_prenhes':
-                // Campos: Nome do Animal, Data Provavel, Data de exame, nome do Pai
                 $data['date_exame'] = $request->input('date_exame');
                 $data['date_provavel'] = $request->input('date_provavel');
 
-                if ($request->input('useAnimalRegistered') && $request->input('animal_id')) {
+                if ($request->boolean('useAnimalRegistered') && $request->filled('animal_id')) {
                     $data['animal_id'] = $request->input('animal_id');
                 } else {
                     $data['animal_name'] = $request->input('animal_name');
                 }
 
-                if ($request->input('usePaiRegistered') && $request->input('pai_id')) {
+                if ($request->boolean('usePaiRegistered') && $request->filled('pai_id')) {
                     $data['pai_id'] = $request->input('pai_id');
                 } else {
                     $data['pai_name'] = $request->input('pai_name');
                 }
-
                 break;
 
             default:
-                // Caso não seja um type esperado
                 return redirect()->back()->withErrors(['type' => 'Tipo de reprodução inválido.']);
         }
 
